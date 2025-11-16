44f89ef139b33761d3609840ec455517adc0918b
Bhoomi Ladia <bhoomiladia2@gmail.com>
2025-11-16 17:48:48 +0530

    utils.py

--- utils.py
+++ utils.py
@@ -1,10 +1,8 @@
 def validate_inputs(stock, quantity, price):
-    """Validates the user inputs from the GUI."""
     if not stock or not quantity or not price:
         return "Please fill all fields!"
 
     try:
-        # We allow negative quantity for selling stocks
         q = int(quantity)
     except ValueError:
         return "Quantity must be an integer (e.g., 100 or -50)."
